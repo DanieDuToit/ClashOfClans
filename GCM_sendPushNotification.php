35fc67f74fdcb7457af4caa245a2ecc763f75a96
DanieDuToit <danie_dutoit@outlook.com>
2015-07-06 08:56:19 +0200

    2 Jul 2015

---
+++ GCM_sendPushNotification.php
@@ -0,0 +1,34 @@
+<?php
+    require_once('GCM_Loader.php');
+    $db = new BaseDB();
+
+    $message  = $_REQUEST["message"];
+    $gameName = $_REQUEST["gameName"];
+
+    $i   = 0;
+    $sql = "";
+    if ($gameName == "all") {
+        $sql = "
+      SELECT gcm_regid, game_name FROM dbo.gcm_users
+    ";
+    } else {
+        $sql = "
+      SELECT gcm_regid, game_name FROM dbo.gcm_users WHERE game_name = '$gameName'
+    ";
+    }
+
+    $result = $db->dbQuery($sql);
+    $data   = array();
+
+    $registatoin_ids = array();
+    $i               = 0;
+    while ($record = sqlsrv_fetch_array($result, SQLSRV_FETCH_BOTH)) {
+        $s                     = $message;
+        $s1                    = $record['gcm_regid'];
+        $registatoin_ids[$i++] = $record['gcm_regid'];
+    }
+    $db->Free($result);
+    $db->close();
+    $msg    = array("data" => $message);
+    $result = send_push_notification($registatoin_ids, $msg);
+?>
